e334e1c83fceea3a300ae745ab09e6c5b1785adc
Eric Chen <eschen@cpp.edu>
2021-09-26 17:39:30 -0700

    added small change to main

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ float savedAmount;
 int main(){
     totalExpenses = totalTravelFee();
     totalAllowableExpenses = totalAllowance();
-    excessExpenses = (totalExpenses - totalAllowableExpenses);
     savedAutomotiveAllowance();
     savedAmount = returnSavedParking() + returnSavedTaxi();
+    excessExpenses = (totalExpenses - totalAllowableExpenses - savedAmount);
     printf("The total expenses for the trip are: $%.2f \n", totalExpenses);
     printf("The total allowable expenses for the trip are: $%.2f \n", totalAllowableExpenses);
     printf("The excess expenses that must be reimbursed are: $%.2f \n", excessExpenses);
